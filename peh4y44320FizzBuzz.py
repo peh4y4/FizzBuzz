fedb7e5262df2c6ff4b58d67563af7aaa93b9955
peh4y4 <89429413+peh4y4@users.noreply.github.com>
2022-07-01 13:43:46 -0500

    Add files via upload

---
+++ peh4y44320FizzBuzz.py
@@ -0,0 +1,19 @@
+from itertools import count
+
+
+startNumber = 0
+countNumber = 0
+endNumber = 100
+
+while countNumber == startNumber or countNumber <= endNumber:
+    countNumber += 1
+    if countNumber % 3 == 0 and countNumber % 5 == 0:
+        print(str(countNumber) + " FizzBuzz")
+        continue
+    elif countNumber % 5 == 0:
+        print(str(countNumber) + " Buzz")
+        continue
+    elif countNumber % 3 == 0:
+        print(str(countNumber) + " Fizz")
+    else:
+        print(str(countNumber))
